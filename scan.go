b9fbac02dcee7fb55eb260c52106eaca35ee61e2
huazhimeng <2902151499@qq.com>
2025-02-12 16:50:33 +0800

    提交

--- scan.go
+++ scan.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"log"
+	"reflect"
 )
 
 func scan[T any](rows pgx.Rows) (res []T, err error) {
-	if res, err = pgx.CollectRows[T](rows, pgx.RowToStructByPos[T]); err != nil {
-		fmt.Printf("CollectRows error: %v", err)
-		return
+	var obj T
+	typ := reflect.TypeOf(obj)
+	//typ := reflect.TypeOf((*T)(nil)).Elem()
+	if typ.Kind() == reflect.Struct {
+		if res, err = pgx.CollectRows[T](rows, pgx.RowToStructByPos[T]); err != nil {
+			fmt.Printf("CollectRows error: %v", err)
+			return
+		}
+	} else {
+		for rows.Next() {
+			if err = rows.Scan(&obj); err != nil {
+				log.Println(err)
+				return
+			}
+			res = append(res, obj)
+		}
 	}
+
 	return
 }
 
